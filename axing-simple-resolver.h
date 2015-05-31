38d40145be28aed4eff14750a774a0437aa2ea19
Emmanuele Bassi <ebassi@gnome.org>
2015-05-31 14:21:36 +0100

    simple-resolver: Modernise GObject code
    Make SimpleResolver a 'final' type; this is more in line with GIO,
    and makes the code simpler.

--- axing-simple-resolver.h
+++ axing-simple-resolver.h
@@ -29,37 +29,10 @@
 G_BEGIN_DECLS
 
 #define AXING_TYPE_SIMPLE_RESOLVER            (axing_simple_resolver_get_type ())
-#define AXING_SIMPLE_RESOLVER(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), AXING_TYPE_SIMPLE_RESOLVER, AxingSimpleResolver))
-#define AXING_SIMPLE_RESOLVER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), AXING_TYPE_SIMPLE_RESOLVER, AxingSimpleResolverClass))
-#define AXING_IS_SIMPLE_RESOLVER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), AXING_TYPE_SIMPLE_RESOLVER))
-#define AXING_IS_SIMPLE_RESOLVER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), AXING_TYPE_SIMPLE_RESOLVER))
-#define AXING_SIMPLE_RESOLVER_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), AXING_TYPE_SIMPLE_RESOLVER, AxingSimpleResolverClass))
 
-typedef struct _AxingSimpleResolver         AxingSimpleResolver;
-typedef struct _AxingSimpleResolverPrivate  AxingSimpleResolverPrivate;
-typedef struct _AxingSimpleResolverClass    AxingSimpleResolverClass;
+G_DECLARE_FINAL_TYPE (AxingSimpleResolver, axing_simple_resolver, AXING, SIMPLE_RESOLVER, AxingResolver)
 
-struct _AxingSimpleResolver {
-    AxingResolver parent;
-
-    /*< private >*/
-    AxingSimpleResolverPrivate *priv;
-};
-
-struct _AxingSimpleResolverClass {
-    AxingResolverClass parent_class;
-
-    /*< private >*/
-    void (*_axing_reserved1) (void);
-    void (*_axing_reserved2) (void);
-    void (*_axing_reserved3) (void);
-    void (*_axing_reserved4) (void);
-    void (*_axing_reserved5) (void);
-};
-
-GType            axing_simple_resolver_get_type         (void);
-
-AxingResolver *  axing_simple_resolver_new              (void);
+AxingResolver * axing_simple_resolver_new       (void);
 
 G_END_DECLS
 
